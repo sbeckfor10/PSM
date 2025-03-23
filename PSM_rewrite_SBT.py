5d94638e008589ee20edab22733548d69356daca
sbeckfor10 <141409128+sbeckfor10@users.noreply.github.com>
2025-03-23 17:29:13 +0000

    Create PSM_rewrite_SBT.py

---
+++ PSM_rewrite_SBT.py
@@ -0,0 +1,15 @@
+# By Simeon Beckford-Tongs BSc MSc Copyright © 2025. All rights reserved. 
+class sphere:
+    def__init__(self, particle_type, speed, mass, colour)
+        self.speed = speed
+        self.mass = mass
+        self.colour = colour
+        self.type = type
+        
+AVNP = (Cu, 100, 1000, blue)
+
+T4 = (DNA, 100, 1000, red)
+
+MS2 = (RNA, 100, 1000, orange)
+
+PRRS = (PRRS, 100, 1000, green)
